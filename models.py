bc7491a1696835be609e836ea4024f41981aad26
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-09-07 23:22:18 +0000

    set up models for the Item object

--- models.py
+++ models.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float
+# import Base from database.py
+from database import Base
 
-#pydantic is used for data validation 
-
-class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    quantity: int
+# defining database models
+class Item(Base):
+    __tablename__ = 'items'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    # index=True ensures that a unique index is set for the column to help in faster sql queries
+    name = Column(String, unique=True, index=True) 
+    # nullable=True ensures that the description column is optional
+    description = Column(String, nullable=True)
+    price = Column(Float, nullable=True)
+    quantity = Column(Integer, nullable=True)
+    
